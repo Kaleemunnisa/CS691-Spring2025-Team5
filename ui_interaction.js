3ceb0d1ce1a4c69ff2814c3e9b9a9de008a7c9d1
sathvik077 <sathviksamineni798@gmail.com>
2025-03-02 15:06:12 -0500

    Updating the latest file

--- ui_interaction.js
+++ ui_interaction.js
@@ -1,2 +1,24 @@
 /* The JavaScript file which is responsible in extracting the meaningful information and process them to backend
 Also file is responsible to display the reports or inspect the insights from backend to html page */
+document.getElementById('generateReport').addEventListener('click', async () => {
+    const startDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value;
+  
+    if (!startDate || !endDate) {
+      alert('Please select both start and end dates to fetch the summary within timeframe.');
+      return;
+    }
+    
+
+    
+    // Send dates to background.js
+    chrome.runtime.sendMessage({ action: 'generateReport', startDate, endDate }, (response) => {
+      const reportResult = document.getElementById('reportResult');
+      preReportNote.innerHTML ='Fetching the emails and processing....';
+      if (response.success) {
+        reportResult.innerHTML = `<a href="${response.reportUrl}" download="report.txt">Download Report</a>`;
+      } else {
+        reportResult.innerHTML = 'Error generating report.';
+      }
+    });
+  });
